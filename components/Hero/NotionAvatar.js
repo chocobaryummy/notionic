a30b7ffad6c12e40f7e5facd76e053f2749c5727
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2022-12-01 16:12:36 +0700

    Update NotionAvatar.js

--- components/Hero/NotionAvatar.js
+++ components/Hero/NotionAvatar.js
@@ -8,7 +8,7 @@ const NotionAvatar = (props) => (
       alignSelf: 'center',
       transform: 'scale(-1,1)'
     }}
-    viewBox='-100 -100 1500 1500'
+    viewBox='0 0 1300 1300'
     xmlns='http://www.w3.org/2000/svg'
     fill='currentColor'
     {...props}
