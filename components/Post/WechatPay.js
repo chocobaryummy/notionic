a3830f6220f215ed7b213824bb173af7d20f9c43
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2022-12-01 14:50:39 +0700

    Update WechatPay.js

--- components/Post/WechatPay.js
+++ components/Post/WechatPay.js
@@ -5,7 +5,7 @@ const WechatPay = () => {
   return (
     <div className='fixed inline-flex shadow-lg bg-gray-100 dark:bg-gray-400 p-5 rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
       <Image
-        src='/wechat-pay.png'
+        src='/QRIS.jpg'
         alt='WeChat Pay'
         width={200}
         height={200}
