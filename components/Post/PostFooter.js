041ae1ce750f8400cb13d2856ae246a69d2724fe
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2022-12-26 08:46:58 +0700

    Update PostFooter.js

--- components/Post/PostFooter.js
+++ components/Post/PostFooter.js
@@ -12,14 +12,14 @@ const PostFooter = () => {
 
   return (
     <div className='w-full pb-12 justify-between font-medium text-gray-500 dark:text-gray-400'>
-      <div className='flex flex-wrap sm:flex-nowrap sm:justify-between items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-700 relative gap-3 px-4 py-3'>
+      <div className='flex flex-wrap sm:flex-nowrap sm:justify-between items-center justify-center rounded-lg bg-gray-300 dark:bg-gray-900 relative gap-3 px-4 py-3'>
         <div className='w-full sm:w-auto max-w-screen-sm inline-block text-sm font-light md:text-base mb-2 sm:mb-0'>
           {t.LAYOUT.NOTICE_TEXT}
         </div>
         <div className='flex flex-wrap gap-3'>
           <Link href={'https://ko-fi.com/N4N3FYZ80'}>
             <button
-              className='flex gap-1 bg-gray-300 dark:bg-gray-900 hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
+              className='flex gap-1 bg-gray-200 dark:bg-gray-700 hover:bg-gray-400 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="1.25rem" height="1.25rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734c4.352.24 7.422-2.831 6.649-6.916zm-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09c-.443-.441-3.368-3.049-4.034-3.954c-.709-.965-1.041-2.7-.091-3.71c.951-1.01 3.005-1.086 4.363.407c0 0 1.565-1.782 3.468-.963c1.904.82 1.832 3.011.723 4.311zm6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015z"/>
@@ -29,7 +29,7 @@ const PostFooter = () => {
           </Link>
           <button
             onClick={() => router.push(BLOG.path || '/contact')}
-            className='flex gap-1 bg-gray-300 dark:bg-gray-900 hover:bg-gay-00 dark:hover:bg-gray-700 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
+            className='flex gap-1 bg-gray-200 dark:bg-gray-700 hover:bg-gray-400 dark:hover:bg-gray-500 hover:text-gray-600 dark:hover:text-gray-300 text-sm rounded-lg px-4 py-2'
           >
             <MailIcon className='flex flex-col justify-center items-center select-none cursor-pointer relative w-5 h-5' />
             {t.LAYOUT.NOTICE_BUTTON}
